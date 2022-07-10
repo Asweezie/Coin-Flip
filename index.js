b402d438ea5073054458105bbcfd8df1a23a5e59
brian <brianduong321@gmail.com>
2022-07-10 10:17:12 -0700

    first version

---
+++ index.js
@@ -0,0 +1,39 @@
+document.querySelector('#flip').addEventListener('click', flipCoin)
+document.querySelector('#flip').addEventListener('click', flipCoinSound)
+document.querySelector('#flip').addEventListener('click', flipCoinGif)
+
+function flipCoin() {
+    let rng = Math.random()
+    if (rng < .5) {
+        setTimeout(function(){
+            let value = document.querySelector('#headsCounter').innerHTML
+        value++
+        document.querySelector('#result').src = 'heads.png';
+        document.querySelector('#headsCounter').innerHTML = value;
+        }, 750)
+        
+    }
+    else {
+        setTimeout(function() {
+            let value = document.querySelector('#tailsCounter').innerHTML
+            value++
+            document.querySelector('#result').src = 'tails.jpg';
+            document.querySelector('#tailsCounter').innerHTML = value;
+        }, 750)
+        
+    }
+}
+
+// const flipCoinSound = function() {
+//     const audio = new Audio('coinflip.mp3')
+//     audio.play();
+// }
+
+function flipCoinSound() {
+    const audio = new Audio('coinflip.mp3')
+    audio.play();
+}
+
+function flipCoinGif() {
+    document.querySelector('#result').src = 'flip.gif';
+}
